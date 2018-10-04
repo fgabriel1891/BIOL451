ed913f4f26da70042d56a2f2ce88761c291e324c
Gabriel Muñoz <fgabriel1891@gmail.com>
2018-10-04 16:57:35 -0400

    update analisis Oct4

--- DataAnalisis.R
+++ DataAnalisis.R
@@ -31,6 +31,7 @@ plot(dataPeso$WetWeight~dataPeso$LitterType)
 plot(dataPeso$DryWeight~dataPeso$LitterType)
 plot(canopy$Site, canopy$Percentage)
 
+
 # Calculate total weights per sample 
 
 WetWeight = aggregate(dataPeso$WetWeight, by =  list(dataPeso$Sample), FUN = sum)
@@ -59,14 +60,6 @@ dataPeso$pesoDryProp = dataPeso$DryWeight/dataPeso$pesoTotDry
 ## Calculate environmental variable Matrix with proportional dry weight (make dummy variables)
 envData = data.frame(reshape2::acast(data = dataPeso, Sample~LitterType, sum, value.var = "pesoDryProp"))
 
-##
-paletteBarPlot = c('#1b9e77','#d95f02','#7570b3','#e7298a','#66a61e','#e6ab02','#a6761d','#666666')
-barplot(t(envData), col = paletteBarPlot)
-
-
-
-
-
 ## Add humidity and canopy op data to envmatrix
 envData$HumStd = Weights$HumStd # Is better to use std humidity or raw humidity
 envData$Canopy = Weights$canopy 
@@ -83,8 +76,113 @@ specTable = reshape2::acast(Sample~Morfospecies, sum, value.var = c("Abundance")
 
 dim(specTable) # dimensions
 
-## Ordinations 
+##
+pdf("figs/Figure1.pdf")
+par(las = 1, 
+    mar=c(5.1, 5.1, 4.1, 8.1), # mar argument of par() means the margins 
+    xpd=TRUE) # parametros 
+paletteBarPlot = c("#c1883f", "#cc554f", "#80a33f", "#95c3ff", "#ccb54e", "#92e473", "#7a7f55", "#1f2a0a")
+barplot(t(envData), # data to plot
+        col = paletteBarPlot, # "col" argument means a vector of colors with the same length of the rownames from the data 
+        xlab = "site",
+        ylab = "litter dry relative abundance",
+        cex.lab = 1.5) 
+legend("topright", 
+       rownames(t(envData)), # rownames =  
+       fill = paletteBarPlot, # fill = 
+       inset=c(-0.32,0))
+dev.off() #
 
+
+#############
+
+## Diversity index of litter
+
+envData3 = envData[1:8]
+
+simpsonDiv = vegan::diversity(envData3, index = "shannon") 
+specRich = specnumber(specTable)
+
+relData = data.frame( simpsonDiv,specRich)
+relData$abundance = rowSums(specTable)
+
+
+divRel = data.frame(simpsonDiv, specRich)
+divRel = divRel[order(divRel$simpsonDiv),]
+
+
+# lets make a regression 
+lm1 = lm(specRich~simpsonDiv)
+lm2 = lm(relData$abundance~relData$simpsonDiv)
+
+pdf("figs/litterComplexityRel.pdf")
+par(las = 1, 
+    mar=c(5.1, 5.1, 2.1, 2.1))
+plot(relData$specRich~relData$simpsonDiv,
+     xlab ="litter complexity (Simpson)",
+     ylab = "number of species",
+     col = "#73b34a", 
+     pch = 16, 
+     cex.lab = 2,
+     cex = log(relData$abundance)
+     )
+points(relData$specRich~relData$simpsonDiv,
+       cex = log(relData$abundance))
+dev.off()
+
+summary(lm1) # species richness
+summary(lm2) # species abundance
+
+## Scatterplot of humidity vs diversity 
+relData$hum = envData$HumStd
+# make regressions 
+humSpeRich = lm(relData$specRich~relData$hum)
+summary(humSpeRich)
+humAbudRel = lm(relData$abundance~relData$hum)
+summary(humAbudRel)
+
+pdf("figs/humidityRel.pdf")
+par(las = 1, 
+    mar=c(5.1, 5.1, 2.1, 2.1))
+plot(relData$specRich~relData$hum,
+     xlab = "relative humidity",
+     ylab = "number of species",
+     col = "#73b34a", 
+     pch = 16, 
+     cex.lab = 2,
+     cex = log(relData$abundance)
+)
+points(relData$specRich~relData$hum,
+       cex = log(relData$abundance))
+dev.off()
+
+
+## Scatterplot of Canopy vs diversity 
+relData$canopy = canopy[match(rownames(relData),canopy$Site),]$Percentage
+
+CanoSpRic = lm(relData$specRich~relData$canopy)
+summary(CanoSpRic)
+CanoSpAbun = lm(relData$abundance~relData$canopy)
+summary(CanoSpAbun)
+# make regression 
+pdf("figs/Canopy.pdf")
+par(las = 1, 
+    mar=c(5.1, 5.1, 2.1, 2.1))
+plot(relData$specRich~relData$canopy,
+     xlab = "relative humidity",
+     ylab = "number of species",
+     col = "#73b34a", 
+     pch = 16, 
+     cex.lab = 2,
+     cex = log(relData$abundance)
+)
+points(relData$specRich~relData$canopy,
+       cex = log(relData$abundance))
+dev.off()
+
+##################
+
+## Ordinations 
 ## Make constrained ordination 
 ccaMatrix = vegan::rda(specTable, envData)
 plot(ccaMatrix)
@@ -116,28 +214,3 @@ summary(ccaMatrix3)
 
 #############
 
-## Diversity index of litter
-
-envData3 = envData[1:8]
-
-simpsonDiv = vegan::diversity(envData3, index = "shannon") 
-specRich = specnumber(specTable)
-
-
-divRel = data.frame(simpsonDiv, specRich)
-divRel = divRel[order(divRel$simpsonDiv),]
-
-
-# lets make a regression 
-lm1 = lm(specRich~simpsonDiv)
-
-par(las=1)
-plot(specRich~simpsonDiv,
-     xlab ="Litter Complexity (Simpson)",
-     ylab = "Number of Species",
-     col = "#73b34a", 
-     pch = 16, 
-     cex = 2
-     )
-abline(lm1)
-anova(lm1)
